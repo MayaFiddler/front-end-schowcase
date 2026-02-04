83e1e0bdc68b15f286a1e457d3f2f7a484a6e9cb
MayaFiddler <63075126+MayaFiddler@users.noreply.github.com>
2026-02-04 08:40:27 +0100

    Adding cash.py file

---
+++ python-scripts/cs50-exercises/indoor.py
@@ -0,0 +1,16 @@
+
+"""
+indoor.py
+
+Converts user input text to lowercase.
+
+Purpose:
+- Practice string methods
+- Understand basic text transformation
+"""
+
+
+#call for user input
+writing = str(input())
+#print input in lower case
+print(writing.lower())
